c3250470f32e0a1aa5eba63c00340588f2fe685d
KmolYuan <daan0014119@gmail.com>
2017-03-22 18:43:49 +0800

    appendix

--- markdown/master.md
+++ markdown/master.md
@@ -38,6 +38,9 @@ abstract: |
 關於數學式可以參考這裡：[http://www.hostmath.com/][]
 
 [http://www.hostmath.com/]: http://www.hostmath.com/
+
+提及了某篇刊物[@myart]在這裡。
+
 大標題二
 ===
 
--- markdown/script.py
+++ markdown/script.py
@@ -4,8 +4,8 @@ elif platform.system().lower()=="windows": font = "DFKai-SB"
 else: font = "Arial"
 fontsize = "12"
 margin = "1in"
-settingFlag = "--latex-engine=xelatex -V lang=chinese -N --toc --highlight-style kate -V documentclass=report  --filter pandoc-fignos --filter pandoc-tablenos --template=template.tex -V \"CJKmainfont:{0}\" -V fontsize={1}pt -V geometry:margin={2}".format(font, fontsize, margin)
+settingFlag = "--latex-engine=xelatex -V lang=chinese -N --toc --highlight-style kate -V documentclass=report  --filter pandoc-fignos --filter pandoc-tablenos --template=template.tex -V \"CJKmainfont:{0}\" -V fontsize={1}pt -V geometry:margin={2} --bibliography=refer.bib --csl=ieee.csl".format(font, fontsize, margin)
 print("---Pandoc---")
-os.system("pandoc master.md paragraph*.md -o ../pdf/report.pdf {}".format(settingFlag))
+os.system("pandoc master.md paragraph*.md reference.md --include-after-body=appendix.md -o ../pdf/report.pdf {}".format(settingFlag))
 g.es("PDF 轉換完畢")
 print('-'*12)
