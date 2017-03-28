1fc9b034fe66ca5a19eaca2f4a05aa576cc09e91
KmolYuan <daan0014119@gmail.com>
2017-03-28 18:37:09 +0800

    Remove include-after.

--- markdown/script.py
+++ markdown/script.py
@@ -6,6 +6,6 @@ fontsize = "12"
 margin = "1in"
 settingFlag = "--latex-engine=xelatex -V lang=chinese -N --toc --highlight-style kate -V documentclass=report  --filter pandoc-fignos --filter pandoc-tablenos --template=template.tex -V \"CJKmainfont:{0}\" -V fontsize={1}pt -V geometry:margin={2} --bibliography=refer.bib --csl=ieee.csl".format(font, fontsize, margin)
 print("---Pandoc---")
-os.system("pandoc master.md paragraph*.md reference.md --include-after-body=appendix.md -o ../pdf/report.pdf {}".format(settingFlag))
+os.system("pandoc master.md paragraph*.md reference.md -o ../pdf/report.pdf {}".format(settingFlag))
 g.es("PDF 轉換完畢")
 print('-'*12)
